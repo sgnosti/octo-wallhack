0d2045d75f2b7483f424fcb530e008558ca13d3a
Sara Gomez Nosti <saragn@gmail.com>
2017-06-30 17:13:21 +0200

    deserialize and serialize again to show discrepance

--- wallhack-data/src/test/java/de/sgnosti/wallhack/integrationtest/TweetModelTest.java
+++ wallhack-data/src/test/java/de/sgnosti/wallhack/integrationtest/TweetModelTest.java
@@ -3,7 +3,10 @@ package de.sgnosti.wallhack.integrationtest;
 import static org.junit.Assert.*;
 
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,27 +16,35 @@ import de.sgnosti.wallhack.model.Tweet2;
 
 public class TweetModelTest {
 
+	private final URL urlFirstExample = getClass().getClassLoader().getResource("tweet_example.json");
+	private final URL urlSecondExample = getClass().getClassLoader().getResource("tweet_example02.json");
+	private String camelCaseExample;
+	private String snakeCaseExample;
+
+	@Before
+	public void setUp() throws Exception {
+		camelCaseExample = new String(Files.readAllBytes(Paths.get(urlFirstExample.toURI())));
+		snakeCaseExample = new String(Files.readAllBytes(Paths.get(urlSecondExample.toURI())));
+	}
+
 	@Test
 	public void firstTweetCanBeRead() throws Exception {
 		final ObjectMapper objectMapper = new ObjectMapper();
-		final URL url = getClass().getClassLoader().getResource("tweet_example.json");
-		final Tweet tweet = objectMapper.readValue(url , Tweet.class);
+		final Tweet tweet = objectMapper.readValue(camelCaseExample, Tweet.class);
 		assertNotNull(tweet.getId());
 	}
 
 	@Test
 	public void secondTweetCanBeRead() throws Exception {
 		final ObjectMapper objectMapper = new ObjectMapper();
-		final URL url = getClass().getClassLoader().getResource("tweet_example02.json");
-		final Tweet2 tweet = objectMapper.readValue(url, Tweet2.class);
+		final Tweet2 tweet = objectMapper.readValue(snakeCaseExample, Tweet2.class);
 		assertNotNull(tweet.getId());
 	}
 
 	@Test
 	public void firstExampleWithSecondObjectTypeDoesNotWork() throws Exception {
 		final ObjectMapper objectMapper = new ObjectMapper();
-		final URL url = getClass().getClassLoader().getResource("tweet_example.json");
-		final Tweet2 tweet = objectMapper.readValue(url, Tweet2.class);
+		final Tweet2 tweet = objectMapper.readValue(camelCaseExample, Tweet2.class);
 		assertNotNull(tweet.getId());
 		// TODO: what is missing?
 	}
@@ -41,10 +52,26 @@ public class TweetModelTest {
 	@Test
 	public void secondExampleWithFirstObjectTypeDoesNotWork() throws Exception {
 		final ObjectMapper objectMapper = new ObjectMapper();
-		final URL url = getClass().getClassLoader().getResource("tweet_example02.json");
-		final Tweet tweet = objectMapper.readValue(url, Tweet.class);
+		final Tweet tweet = objectMapper.readValue(snakeCaseExample, Tweet.class);
 		assertNotNull(tweet.getId());
 		// TODO: what is missing?
 	}
 
+	@Test
+	public void comparisonOnFirstTweet() throws Exception {
+		final ObjectMapper objectMapper = new ObjectMapper();
+		final Tweet tweet = objectMapper.readValue(camelCaseExample, Tweet.class);
+		final String s = objectMapper.writeValueAsString(objectMapper.readValue(camelCaseExample, Tweet2.class));
+		final Tweet tweet2 = objectMapper.readValue(s, Tweet.class);
+		assertEquals(tweet, tweet2);
+	}
+
+	@Test
+	public void comparisonOnSecondTweet() throws Exception {
+		final ObjectMapper objectMapper = new ObjectMapper();
+		final Tweet2 tweet = objectMapper.readValue(snakeCaseExample, Tweet2.class);
+		final String s = objectMapper.writeValueAsString(objectMapper.readValue(snakeCaseExample, Tweet.class));
+		final Tweet tweet2 = objectMapper.readValue(s, Tweet.class);
+		assertEquals(tweet, tweet2);
+	}
 }
